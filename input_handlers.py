2ff8f159f29517347ff9b982d3e47d8ffd319bda
dunkacinno <43553754+dunkacinno@users.noreply.github.com>
2018-09-26 14:53:24 -0700

    2nd commit

---
+++ input_handlers.py
@@ -0,0 +1,39 @@
+import tcod as libtcod
+
+
+def handle_keys(key):
+    # Movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move': (1, -1)}
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move': (1, 1)}
+    elif key.vk == libtcod.KEY_KP8:
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_KP2:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_KP4:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_KP6:
+        return {'move': (1, 0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
+
+    # No key was pressed
+    return {}
